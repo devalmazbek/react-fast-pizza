e6bb2a6371ce8474a8af50d79303f59e4c335b10
devalmazbek <devalmazbek@gmail.com>
2024-12-23 17:15:43 +0600

    implementation Error handling action data react router dom

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -31,6 +33,12 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const formErrors = useActionData();
+
+  const isSubmittitng = navigation.state === 'submitting';
+
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -38,7 +46,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -48,6 +56,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors?.phone}</p>}
           </div>
         </div>
 
@@ -70,11 +79,41 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
+
+          <button disabled={isSubmittitng}> {isSubmittitng ? 'Please wait...' : 'Order now'}</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+
+export async function action({request}) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  }
+
+  const error = {};
+
+  if(!isValidPhone(order.phone)) {
+    error.phone = "Please write correct phone number. We need it to contact you!";
+  }
+
+  if(Object.keys(error).length > 0) {
+    return error
+  };
+
+  // if everything is okay, create new order and ridirect  
+  const newOrder = await createOrder(order);
+  console.log(newOrder);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
