b741ca76df9539abf3effd43e147a4361eec6ec6
devalmazbek <devalmazbek@gmail.com>
2024-12-25 09:37:20 +0600

    implementation: styles

---
+++ src/features/users/User.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function User() {
+  return (
+    <div className='text-sm font-semibold'>Diamond</div>
+  )
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Search from '../features/order/Search'
+import User from '../features/users/User'
 
 export default function Header() {
   return (
-    <header className='bg-yellow-500'>
-        <Link to='/'>Fast React Pizza Co.</Link>
+    <header className='flex items-center justify-between bg-yellow-500 uppercase border-b border-stone-200 px-4 py-3'>
+        <Link to='/' className='tracking-widest'>Fast React Pizza Co.</Link>
         <Search />
-        <p>Pizza Fast</p>
+        <User />
     </header>
   )
 }
