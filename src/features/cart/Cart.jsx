4bd1b0b0caba70dc119b81987b3539f5c8832601
devalmazbek <devalmazbek@gmail.com>
2024-12-30 09:24:24 +0600

    implementation: styles for components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import CartItem from "./CartItem"
+
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 
@@ -33,11 +35,15 @@ function Cart() {
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
-      <h2 >Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-lg font-semibold'>Your cart, %NAME%</h2>
 
-      <div>
-        <Button to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+      <ul className="mt-3 mb-6 divide-y divide-stone-200 border-b">
+        {cart.map((item) => <CartItem item={item} key={item.pizzaId} />)}
+      </ul>
+
+      <div className='space-x-3'>
+        <Button to="/order/new" type="primary">Order pizzas</Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
